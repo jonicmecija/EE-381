87233cc38a7d2e468a25456b7ee5347652d56d2f
Jonic Mecija <jonic.mecija@yahoo.com>
2019-09-18 09:42:40 -0700

    Update EE381_Proj2_2.py

--- EE381_Proj2_2.py
+++ EE381_Proj2_2.py
@@ -7,29 +7,52 @@ Jonic Mecija
 Start Date: September 16, 2019
 End Date:
 
-Description: Write te code for the pseudorandom number generator (RNG).
- Use the RNG to solve probability problems. This is problem 1.
+Description: Problem 2.
 '''
 import math
 import time
 
-t = time.process_time()
+def main():
+        success = 0
+        failure = 0
 
-#Constants for RNG
-N = 100000 # the norm
-A = 4857 # the adder
-M = 8601 # the multiplier
+        for i in range(1000000):
+                x = diceroll()
 
-for i in range(10):
-        S_1 = int(10000*t)
-        S_1 = (M * S_1 + A) % N
-        r = S_1/N
-        die_1 = math.floor(6*r+1)
+                try:
+                        three = x.index(3)
+                        a = three + 1
+                        success = success + a
+                except:
+                        b = len(x)
+                        failure += b
+        p = success / (success + failure)
+        print(p)
 
-        S_2 = int(10000*t)
-        S_2 = (M * S_2 + A) % N
-        r = S_2/N
-        die_2 = math.floor(6*r+1)
+def diceroll():
+        #Constants for RNG
+        N = 100000 # the norm
+        A = 4857 # the adder
+        M = 8601 # the multiplier
 
-        
-        print(die_1, die_2)
+        Z = 0
+        Rolls = []
+
+        while Z != 7:
+                
+                S_1 = time.perf_counter_ns()%999999
+                S_1 = (M * S_1 + A) % N
+                r = S_1/N
+                die_1 = math.floor(6*r+1)
+
+                S_2 = time.perf_counter_ns()%10000
+                S_2 = (M * S_2 + A) % N
+                r = S_2/N
+                die_2 = math.floor(6*r+1)
+
+                Z = die_1 +die_2
+                Rolls.append(Z)
+
+        return Rolls
+
+main()
